428f1edd13ddf078c204382dc2b395a89c0ddc2b
HelenXo <126959953+HelenXo@users.noreply.github.com>
2023-03-08 14:42:48 +0500

    сущность Comment

--- src/main/java/com/naumen/blogeng/model/Comment.java
+++ src/main/java/com/naumen/blogeng/model/Comment.java
@@ -1,8 +1,50 @@
 package com.naumen.blogeng.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import org.springframework.lang.NonNull;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+@Entity
 public class Comment {
 
+    private final DateFormat dateFormat = new SimpleDateFormat("s:m:h dd/MM/yyyy");
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NonNull
+    private String textComment;
+    private Date date;
+
+    @ManyToOne
+    @JoinColumn (name = "post_id")
+    private Post post;
+
+    @ManyToOne // Добавила еще зависимость с пользователем
+    @JoinColumn (name = "blog_user_id")
+    private BlogUser blogUser;
+
+    public Comment (@NonNull String textComment, Post post, BlogUser blogUser) {
+        this.textComment = textComment;
+        this.date = dateFormat.getCalendar().getTime();
+        this.post = post;
+        this.blogUser = blogUser;
+    }
+
+    public Comment() {}
+
+    public Long getId() { return id;}
+
+    public Date getDate() {return date;}
+
+    @NonNull
+    public String getTextComment() {return textComment;}
+
+    public void setTextComment(@NonNull String textComment) {
+        this.textComment = textComment;
+    }
 }
--- src/main/java/com/naumen/blogeng/repository/CommentRepository.java
+++ src/main/java/com/naumen/blogeng/repository/CommentRepository.java
@@ -1,7 +1,9 @@
 package com.naumen.blogeng.repository;
 
+import com.naumen.blogeng.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentRepository {
+public interface CommentRepository extends JpaRepository <Comment, Long> {
 }
