de2367acec712d142ddebfc2273db0052f785eb0
Kopotev Nikita <122271491+viaarm@users.noreply.github.com>
2023-04-08 16:55:31 +0500

    Отображение

--- src/main/java/com/naumen/blogeng/config/SecurityConfig.java
+++ src/main/java/com/naumen/blogeng/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -43,9 +44,9 @@ public class SecurityConfig {
         http.csrf().disable()
                 .authorizeHttpRequests((authorize) ->
                         authorize.requestMatchers("/registration/**").permitAll()
-                                .requestMatchers("/").authenticated()
-                                .requestMatchers("/post/**").hasRole("USER")
-                                .requestMatchers("/users").hasRole("ADMIN")
+                                .requestMatchers("/").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/post/**").hasRole("USER")
+                                .requestMatchers(HttpMethod.GET,"/users").hasRole("ADMIN")
                 ).formLogin(
                         form -> form
                                 .loginPage("/login")
